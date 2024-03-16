dcd9a667f50697575ba39859159430f6455de17f
Ro <wondaero@nate.com>
2024-03-16 18:26:34 +0900

    반납페이지(연장 기능 해야함)

--- src/views/admin/Return1.vue
+++ src/views/admin/Return1.vue
@@ -1,28 +1,11 @@
 <script setup>
-  import { reactive, ref, nextTick } from 'vue';
+  import { reactive } from 'vue';
   import axios from 'axios';
-  import commonJS from '../../assets/common';
 
-  const userInput = ref(null);
-  const bookCdeInput = ref(null);
-  const userPop = ref(null);
-  const userList = ref(null);
-  const bookList = ref(null);
-  // const allChkInput = ref(null);
-
-  const state2 = reactive({
-    showedPopup: false,
-    userList: [],
-    selectedUser: '',   //유저 선택시 object로 바꾸기{}
-    userKeyword: '',
-    bookCde: '',
-    bookList: [],
-    allChked: true
-  });
   const state = reactive({
     bookCde: '',
     borrowedBooks: [],
-    allChked: true
+    allChked: true,
   });
 
   const getBeingBorrowedBook = () => {
@@ -46,64 +29,26 @@
 
       state.borrowedBooks.push(bookObj);
 
-      // chkEach();
-    }).catch(e => {
-      console.error(e);
-    })
-  }
-
-  
-
-
-  const getBookInfo = () => {
-    if(state.bookList.filter(x => x.book_cde === state.bookCde).length){
-      alert('이미 목록에 있습니다.');
-      return;
-    }
-
-    axios.post(`http://localhost:3000/getBookFromBorrow`, {
-      bookCde: state.bookCde
-    }).then(res => {
-      if(!res.data.length){
-        alert('이미 대여중인 도서이거나\n존재하지 않는 도서입니다.');
-        return;
-      }
-
-      const bookObj = res.data[0];
-      bookObj.chked = true;
-      
-      var today = new Date();
-      today.setDate(today.getDate() + 14);
-      bookObj.due = setYYYYMMDD(today);
-
-      state.bookList.push(bookObj);
-
-      console.log(state.bookList);
-
       chkEach();
     }).catch(e => {
       console.error(e);
     })
   }
 
-  const borrow = () => {
-    
-  }
-
-  const del = (idx) => {
+  const del = (seq) => {
     if(window.confirm('위 도서를 목록에서 제거하시겠습니까?')){
-      state.bookList.splice(idx, 1);
+      state.borrowedBooks = state.borrowedBooks.filter(x => x.seq != seq);
     }
 
     chkEach();
   }
   const del2 = () => {
-    if(!state.bookList.filter(x => x.chked).length){
+    if(!state.borrowedBooks.filter(x => x.chked).length){
       alert('선택된 도서가 없습니다.');
       return;
     }
     if(window.confirm('선택된 도서를 목록에서 제거하시겠습니까?')){
-      state.bookList = state.bookList.filter(x => x.chked !== true);
+      state.borrowedBooks = state.borrowedBooks.filter(x => x.chked !== true);
     }
 
     chkEach(false);
@@ -111,8 +56,8 @@
 
   const chkedBookCtrl = (trg, cde) => {
     let book;
-    for(let i = 0; i < state.bookList.length; i++){
-      book = state.bookList[i];
+    for(let i = 0; i < state.borrowedBooks.length; i++){
+      book = state.borrowedBooks[i];
       if(book.book_cde == cde){
         book.chked = trg.target.checked;
         break;
@@ -127,7 +72,7 @@
   const isOver = rtnDt => (new Date(rtnDt) - new Date()) < 0;
 
   const chkAll = () => {
-    state.bookList.map(x => x.chked = state.allChked);
+    state.borrowedBooks.map(x => x.chked = state.allChked);
   }
 
   const chkEach = (bool) => {
@@ -136,61 +81,51 @@
       return;
     }
 
-    state.allChked = !state.bookList.filter(x => x.chked === false).length;
+    state.allChked = !state.borrowedBooks.filter(x => x.chked === false).length;
   }
 
-  const borrow1 = (cde) => {
-    // nextTick(() => {
-      console.log('single');
-      const book = state.bookList.filter(x => x.book_cde === cde)[0];
+  const return1 = (seq) => {
+      const book = state.borrowedBooks.filter(x => x.seq === seq);
 
-      if(state.selectedUser.cur_borrowing_cnt + 1 > state.selectedUser.borrowing_limit){ //필터
-        alert('최대 대여수를 넘길 수 없습니다.');
+      if(!book.length){ //필터
+        alert('[rtn001] 반납을 진행할 수 없습니다.\n관리자에게 문의해주세요.');
         return;
       }
       
-      axios.post(`http://localhost:3000/borrow1`, {
-        books: [{
-          bookCde: book.book_cde,
-          userCde: state.selectedUser.user_cde,
-          rtnDate: book.due,
-        }]
+      axios.post(`http://localhost:3000/return1`, {
+        books: book
       }).then((res) => {
-        alert('대여되었습니다.');
-        state.bookList = state.bookList.filter(x => x.book_cde !== cde);
-        state.selectedUser.cur_borrowing_cnt += 1;
+        alert('반납되었습니다.');
+        state.borrowedBooks = state.borrowedBooks.filter(x => x.seq !== seq);
       }).catch((err) => {
         console.log(err);
-        alert('관리자에게 문의해주세요.');
+        alert('[rtn002] 관리자에게 문의해주세요.');
       });
-    // })
-    
   }
 
-  const borrow2 = () => {
-    const books = state.bookList.filter(x => x.chked === true);
+  const return2 = () => {
+      const books = state.borrowedBooks.filter(x => x.chked === true);
 
-    const data = [];
-    for(let i = 0; i < books.length; i++){
-      data.push({
-        bookCde: books[i].book_cde,
-        userCde: state.selectedUser.user_cde,
-        rtnDate: books[i].due,
+      if(!books.length){ //필터
+        alert('선택된 도서가 없습니다.');
+        return;
+      }
+
+      if(!confirm(`선택된 도서(${books.length}권)를 반납하시겠습니까?`)) return;
+      
+      axios.post(`http://localhost:3000/return1`, {
+        books: books
+      }).then((res) => {
+        alert('반납되었습니다.');
+        state.borrowedBooks = state.borrowedBooks.filter(x => x.chked === false);
+      }).catch((err) => {
+        console.log(err);
+        alert('[rtn002] 관리자에게 문의해주세요.');
       });
-    }
+  }
 
-    if(data.length +  state.selectedUser.cur_borrowing_cnt > state.selectedUser.borrowing_limit){ //필터
-      alert('최대 대여수를 넘길 수 없습니다.');
-      return;
-    }
-
-    axios.post(`http://localhost:3000/borrow1`, {
-      books: data
-    }).then((res) => {
-      alert('대여되었습니다.');
-      state.bookList = state.bookList.filter(x => x.chked === false);
-      state.selectedUser.cur_borrowing_cnt += data.length;
-    });
+  const extend = () => {
+    alert('아직 사용할 수 없습니다.');
   }
 
 </script>
@@ -202,7 +137,7 @@
         <div class="row">
           <label class="col with-btn">
             <span class="title ast">도서</span>
-            <input ref="userInput" type="text" placeholder="도서코드" @keyup.enter="getBeingBorrowedBook" v-model="state.bookCde" />
+            <input type="text" placeholder="도서코드" @keyup.enter="getBeingBorrowedBook" v-model="state.bookCde" />
             <button class="btn-form btn-search" @click="getBeingBorrowedBook">검색</button>
           </label>
         </div>
@@ -216,11 +151,11 @@
             <div>{{ state.borrowedBooks.filter(x => x.chked).length }} / {{ state.borrowedBooks.length }}</div>
           </div>
           <div class="right">
-            <button class="btn-form del" @click="del2">선택제거</button>
-            <button class="btn-form" @click="borrow2()">선택반납</button>
+            <button class="btn-form btn-del" @click="del2">선택제거</button>
+            <button class="btn-form btn-return" @click="return2()">선택반납</button>
           </div>
         </div>
-        <ul class="list" ref="bookList">
+        <ul class="list">
           <li v-for="(book, idx) in state.borrowedBooks" :key="idx">
             <label>
               <input type="checkbox" @change="chkedBookCtrl($event, book.book_cde)" v-model="book.chked">
@@ -248,8 +183,9 @@
                   </div>
                   <div class="right">
                     <input type="date" v-model="book.org_return_dt" />
-                    <button class="btn-form" @click="borrow1(book.book_cde)">반납</button>
-                    <button class="btn-form del" @click="del(idx)">제거</button>
+                    <button class="btn-form btn-return" @click="return1(book.seq)">반납</button>
+                    <button class="btn-form btn-extend" disabled @click="extend(book.seq)">기간연장</button>
+                    <button class="btn-form btn-del" @click="del(book.seq)">제거</button>
                   </div>
                 </div>
               </div>
@@ -263,97 +199,44 @@
 </template>
 
 <style lang="scss" scoped>
-.return-page{
-  display: flex;
-  flex-direction: column;
+  .return-page{
+    display: flex;
+    flex-direction: column;
 
-  .ast:before{
-    content: '*';
-    margin-right: 10px;
-    font-size: 20px;
-    font-weight: bolder;
-    color: var(--color-red);
-  }
-  .step1{
+    .ast:before{
+      content: '*';
+      margin-right: 10px;
+      font-size: 20px;
+      font-weight: bolder;
+      color: var(--color-red);
+    }
+    .step1{
 
-    .grid{
-      .row{
-        .col{
-          align-items: center;
-          margin-right: 10px;
-          &:last-child{margin-right: 0;}
-          .title{
-            text-align: right;
-            padding-right: 10px;
-            box-sizing: border-box;
+      .grid{
+        .row{
+          .col{
+            align-items: center;
+            margin-right: 10px;
+            &:last-child{margin-right: 0;}
+            .title{
+              text-align: right;
+              padding-right: 10px;
+              box-sizing: border-box;
+            }
+
+            input[type="text"]{
+              width: 300px;
+              height: 32px;
+              padding: 5px 10px;
+            }
+
+            &.with-btn{
+              input{margin-right: 5px;}
+            }
           }
 
-          input[type="text"]{
-            width: 300px;
-            height: 32px;
-            padding: 5px 10px;
-          }
-
-          &.with-btn{
-            input{margin-right: 5px;}
-          }
+          &.btns{justify-content: flex-end;}
         }
-
-        &.btns{justify-content: flex-end;}
-      }
-    }
-
-    button{
-      background: var(--color-green);
-      color: #fff;
-    }
-
-    .btns{
-      margin-top: 20px;
-      padding-top: 10px;
-      border-top: 1px dashed rgba(0, 0, 0, .5);
-    }
-
-    &:after{
-      content: 'STEP 1.';
-      display: block;
-      position: absolute;
-      right: 0;
-      top: 0;
-      background: #ace;
-      transform: translateY(-50%);
-      font-weight: bold;
-    }
-  }
-
-  .step2{
-    margin-top: 20px;
-
-    .grid{
-      .row{
-
-        .col{
-          align-items: center;
-          margin-right: 10px;
-          &:last-child{margin-right: 0;}
-          .title{
-            text-align: right;
-            padding-right: 10px;
-            box-sizing: border-box;
-          }
-          
-          input[type="text"]{
-            width: 300px;
-            height: 32px;
-            padding: 5px 10px;
-          }
-
-          &.with-btn{
-            input{margin-right: 5px;}
-          }
-        }
-
-        &.btns{justify-content: flex-end;}
       }
 
       button{
@@ -366,165 +249,222 @@
         padding-top: 10px;
         border-top: 1px dashed rgba(0, 0, 0, .5);
       }
+
+      &:after{
+        content: 'STEP 1.';
+        display: block;
+        position: absolute;
+        right: 0;
+        top: 0;
+        background: #ace;
+        transform: translateY(-50%);
+        font-weight: bold;
+      }
     }
 
-    &:after{
-      content: 'STEP 2.';
-      display: block;
-      position: absolute;
-      right: 0;
-      top: 0;
-      background: #ace;
-      transform: translateY(-50%);
-      font-weight: bold;
-    }
+    .step2{
+      margin-top: 20px;
 
-    .list-wrapper{
-      
-      margin-top: 10px;
+      .grid{
+        .row{
 
-      .list-top{
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+          .col{
+            align-items: center;
+            margin-right: 10px;
+            &:last-child{margin-right: 0;}
+            .title{
+              text-align: right;
+              padding-right: 10px;
+              box-sizing: border-box;
+            }
+            
+            input[type="text"]{
+              width: 300px;
+              height: 32px;
+              padding: 5px 10px;
+            }
 
-        .left{
+            &.with-btn{
+              input{margin-right: 5px;}
+            }
+          }
+
+          &.btns{justify-content: flex-end;}
+        }
+
+        button{
+          background: var(--color-green);
+          color: #fff;
+        }
+
+        .btns{
+          margin-top: 20px;
+          padding-top: 10px;
+          border-top: 1px dashed rgba(0, 0, 0, .5);
+        }
+      }
+
+      &:after{
+        content: 'STEP 2.';
+        display: block;
+        position: absolute;
+        right: 0;
+        top: 0;
+        background: #ace;
+        transform: translateY(-50%);
+        font-weight: bold;
+      }
+
+      .list-wrapper{
+        
+        margin-top: 10px;
+
+        .list-top{
           display: flex;
           align-items: center;
-          
-          input[type="checkbox"]{
-            width: 20px;
-            height: 20px;
-            margin-right: 10px; 
+          justify-content: space-between;
+
+          .left{
+            display: flex;
+            align-items: center;
+            
+            input[type="checkbox"]{
+              width: 20px;
+              height: 20px;
+              margin-right: 10px; 
+            }
           }
         }
 
+        .btn-form[disabled]{
+          background: #ccc;
+          color: #999;
+          cursor: not-allowed;
+        }
+        .btn-return{
+          background: var(--color-green);
+          color: #fff;
+        }
+        .btn-extend{
+          background: var(--color-orange);
+          color: #fff;
+        }
 
-        .del{
+        .btn-del{
           margin-right: 5px;
           background: var(--color-red);
           color: #fff;
         }
-      }
 
-      .list{
-        // max-height: 200px; //임시
-        overflow-y: auto;
-  
-        list-style: none;
-        margin: 0;
-        margin-top: 10px;
-        padding: 10px;
-  
-        border-top: 1px solid #ccc;
-  
-        margin-bottom: 10px;
-  
-        li{
-
-          margin-bottom: 10px;
-
-          &:last-child{
-            margin-bottom: 0;
-          }
-
-          label{
-            display: block;
-            background: #fafbfc;
-            border-radius: 10px;
-            padding: 10px;
-            border: 1px solid #ddd;
-            
-            transition: color .5s;
-            box-shadow: 0 3px 5px rgba(0, 0, 0, .2);
-
-            input[type="checkbox"]{
-              width: 20px;
-              height: 20px;
-            }
+        .list{
+          // max-height: 200px; //임시
+          overflow-y: auto;
     
-            &:hover{
-              background: var(--color-green);
-              color: #fff;
-
-              .bottom .class-no{
-                color: darkviolet;
-              }
-            }
+          list-style: none;
+          margin: 0;
+          margin-top: 10px;
+          padding: 10px;
+          border-top: 1px solid #ccc;
     
+          li{
+
+            margin-bottom: 10px;
+
+            &:last-child{
+              margin-bottom: 0;
+            }
+
+            label{
+              display: block;
+              background: #fafbfc;
+              border-radius: 10px;
+              padding: 10px;
+              border: 1px solid #ddd;
               
-            .top{
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-    
-              .left{
-                flex: 1;
-                display: flex;
-                margin-right: 5px;
-                width: 10px;
-                white-space: nowrap;
-                overflow: hidden;
-                .book-cde{margin-right: 5px;}
-                .book-title{flex: 1;}
-              }
-              .right.over{
-                animation: blink .5s infinite linear alternate;
-              }
-            }
-      
-            .bottom{
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-    
-              .left{
-                flex: 1;
-                margin-right: 5px;
-              }
-              .class-no{
-                color: #42c7d5;
-              }
-            }
-      
-            .option{
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              margin-top: 10px;
-              padding-top: 10px;
-              border-top: 1px dashed #eee;
-              
-              .right{
-                display: flex;
-              }
+              transition: color .5s;
+              box-shadow: 0 3px 5px rgba(0, 0, 0, .2);
 
-              .del{
-                background: var(--color-red);
+              input[type="checkbox"]{
+                width: 20px;
+                height: 20px;
+              }
+      
+              &:hover{
+                background: var(--color-green);
                 color: #fff;
+
+                .bottom .class-no{
+                  color: darkviolet;
+                }
+
+                .btn-form{
+                  border: 1px solid rgba(255, 255, 255, .8);
+                  box-shadow: 0 0 5px rgba(255, 255, 255, .8);
+                }
               }
-    
-              *{margin-right: 5px;}
-              & *:last-child{margin-right: 0;}
+      
+                
+              .top{
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+      
+                .left{
+                  flex: 1;
+                  display: flex;
+                  margin-right: 5px;
+                  width: 10px;
+                  white-space: nowrap;
+                  overflow: hidden;
+                  .book-cde{margin-right: 5px;}
+                  .book-title{flex: 1;}
+                }
+                .right.over{
+                  animation: blink .5s infinite linear alternate;
+                }
+              }
+        
+              .bottom{
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+      
+                .left{
+                  flex: 1;
+                  margin-right: 5px;
+                }
+                .class-no{
+                  color: #42c7d5;
+                }
+              }
+        
+              .option{
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                margin-top: 10px;
+                padding-top: 10px;
+                border-top: 1px dashed #eee;
+                
+                .right{
+                  display: flex;
+                }
+      
+                *{margin-right: 5px;}
+                & *:last-child{margin-right: 0;}
+              }
             }
           }
         }
       }
     }
+
+    h1{font-size: 20px; margin-bottom: 10px;}
+
   }
 
-  h1{font-size: 20px; margin-bottom: 10px;}
-
-}
-
-@keyframes blink{
-  0%{
-    color: transparent;
+  @keyframes blink{
+    0%{color: transparent;}
+    100%{color: var(--color-red);}
   }
-  100%{
-    color: var(--color-red);
-  }
-}
-  
-
 </style>
